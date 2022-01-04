18338d5e7924f3ccff001e59c3a39d2d142b2979
wahyubudies <wahyubudies1@gmail.com>
2022-01-04 13:40:10 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './js/**/*.js',
+    './index.html',
+    './success.html',
+    './details.html',
+    './cart.html',
+    './404.html',
+    ],
+    theme: {
+    extend: {
+      colors: {
+        'brown-farm': 'rgb(100 53 9 / 76%)'
+      }
+    },
+  },
+  plugins: [],
+}
